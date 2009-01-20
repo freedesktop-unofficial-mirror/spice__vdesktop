a8dadce352754291d78a3fb23fea753b4b4f0cd4
Andrea Arcangeli <aarcange@redhat.com>
2009-01-20 22:26:09 +0100

    kmem-cache
    Add kmem-cache compat code for ksm.c.
    
    Signed-off-by: Izik Eidus <ieidus@redhat.com>
    Signed-off-by: Andrea Arcangeli <aarcange@redhat.com>
    RH-Upstream-status: pending

--- kernel/external-module-compat-comm.h
+++ kernel/external-module-compat-comm.h
@@ -628,6 +628,20 @@ static inline int pci_reset_function(struct pci_dev *dev)
 
 #endif
 
+#ifndef KMEM_CACHE
+/*
+ * Please use this macro to create slab caches. Simply specify the
+ * name of the structure and maybe some flags that are listed above.
+ *
+ * The alignment of the struct determines object alignment. If you
+ * f.e. add ____cacheline_aligned_in_smp to the struct declaration
+ * then the objects will be properly aligned in SMP configurations.
+ */
+#define KMEM_CACHE(__struct, __flags) kmem_cache_create(#__struct,\
+		sizeof(struct __struct), __alignof__(struct __struct),\
+		(__flags), NULL)
+#endif
+
 #include <linux/interrupt.h>
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,20)
 
