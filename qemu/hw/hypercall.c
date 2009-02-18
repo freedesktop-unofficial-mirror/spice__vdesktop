706cadb75e68ed261b969f87a41209a3de4cf4d3
Eduardo Habkost <ehabkost@redhat.com>
2009-02-18 16:28:03 -0300

    vmchannel: use Red Hat's PCI vendor ID
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    RH-Upstream-status: not-applicable
    Bugzilla: 487109
    Acked-by: Dor Laor <dlaor@redhat.com>
    Acked-by: Eduardo Habkost <ehabkost@redhat.com>

--- qemu/hw/hypercall.c
+++ qemu/hw/hypercall.c
@@ -304,8 +304,8 @@ static void pci_hypercall_single_init(PCIBus *bus, uint32_t deviceid, uint32_t i
 
 
     pci_conf = d->dev.config;
-    pci_conf[0x00] = 0x02; // Qumranet vendor ID 0x5002
-    pci_conf[0x01] = 0x50;
+    pci_conf[0x00] = 0xF4; // Red Hat vendor ID 0x1AF4
+    pci_conf[0x01] = 0x1A;
     pci_conf[0x02] = deviceid & 0x00ff;
     pci_conf[0x03] = (deviceid & 0xff00) >> 8;
 
