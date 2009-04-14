ad73e35d63a8cd565f78921c00a89c9a4b595e0d
Izik Eidus <ieidus@redhat.com>
2009-04-14 19:29:18 +0300

    KSM: fix bug 495714
    fixing it by just calling jhash2() instead of jhash()
    
    Signed-off-by: Izik Eidus <ieidus@redhat.com>
    Message-Id: <1239726558-14362-6-git-send-email-ieidus@redhat.com>
    Message-Id: <1239808529-22398-6-git-send-email-ieidus@redhat.com>
    Message-Id: <1240106904-21687-6-git-send-email-ieidus@redhat.com>
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com
    Bugzilla: 500263
    RH-Upstream-status: pending
    Acked-by: Andrea Arcangeli <aarcange@redhat.com>
    Acked-by: "Michael S. Tsirkin" <mst@redhat.com>
    Acked-by: john cooper <john.cooper@redhat.com>

--- kernel/ksm/ksm_main.c
+++ kernel/ksm/ksm_main.c
@@ -243,7 +243,7 @@ static inline u32 calc_checksum(struct page *page)
 {
 	u32 checksum;
 	void *addr = kmap_atomic(page, KM_USER0);
-	checksum = jhash(addr, PAGE_SIZE, 17);
+	checksum = jhash2(addr, PAGE_SIZE / 4, 17);
 	kunmap_atomic(addr, KM_USER0);
 	return checksum;
 }
