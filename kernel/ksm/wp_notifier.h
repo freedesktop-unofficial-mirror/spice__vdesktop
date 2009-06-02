d7c668aed21615f5e036b1c14f50888af0d5aade
Chris Wright <chrisw@redhat.com>
2009-06-01 17:22:40 -0700

    ksm: remove kprobes usage for rhel 5.4 (v3)
    Considering the fact that rhel 5.4 have mmu notifiers support, we dont need
    the kprobes to handle the notifications to kvm.
    
    Leaving the current code as it is - is not a bug, it is just matther of
    unneeded code that hurt the performence for rhel 5.4 when ksm is loaded
    (the copy on write performence)
    
    (Btw, i have faked here the signeture of Chris, hope he will forgive me :))
    
    Thanks.
    
    Message-ID: <20090602002240.GL1494@x200.localdomain>
    Obsoletes: <1243894307-17932-2-git-send-email-ieidus@redhat.com>
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    RH-Upstream-status: not-applicable
    Acked-by: Izik Eidus <ieidus@redhat.com>
    Bugzilla: 504237
    Acked-by: Marcelo Tosatti <mtosatti@redhat.com>
    Signed-off-by: Izik Eidus <ieidus@redhat.com>
    Message-Id: <1244142579-3405-2-git-send-email-ieidus@redhat.com>
    Acked-by: Andrea Arcangeli <aarcange@redhat.com>
    Acked-by: john cooper <john.cooper@redhat.com>

--- kernel/ksm/wp_notifier.h
+++ kernel/ksm/wp_notifier.h
@@ -3,8 +3,13 @@
 
 #include <linux/mm.h>
 
+#ifndef CONFIG_MMU_NOTIFIER
 int init_wp_notifier(void);
 void exit_wp_notifier(void);
+#else
+static inline int init_wp_notifier(void) {return 0;}
+static inline void exit_wp_notifier(void) {return;}
+#endif
 
 void kvm_wp_notifier(struct mm_struct *mm,
                     unsigned long address);
