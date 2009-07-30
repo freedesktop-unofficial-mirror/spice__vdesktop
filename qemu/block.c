32bf73073ae7d6c9b509001ad05f8eeaf20b0f66
Markus Armbruster <armbru@redhat.com>
2009-07-31 00:35:40 +0200

    Restore support for block driver "fat"
    Okay, brown-paperbag-time.  Block drivers have two names.  "fat" is the
    protocol name.  What is actually matched by bdrv_is_supported() is the
    format name, which is "vvfat".
    
    Still bug 514785.
    
    From: Markus Armbruster <armbru@redhat.com>
    Date: Thu, 30 Jul 2009 20:59:16 +0200
    Subject: [PATCH RHEL-5] Restore support for block driver "fat"
    
    When we axed unsupported block drivers, we also killed off "fat".  This
    broke RHEVM.  The need for fat had been mentioned, but was
    overlooked.
    
    Regardless of whether RHEVM should or could be changed not to use "fat",
    we need to unbreak it first, and ask questions later.
    
    Bug 514785.  Please ACK.
    
    Signed-off-by: Markus Armbruster <armbru@redhat.com>
    RH-Upstream-status: not-applicable
    Bugzilla: 514785
    Acked-by: Chris Wright <chrisw@redhat.com>
    Acked-by: Gleb Natapov <gleb@redhat.com>
    Acked-by: Juan Quintela <quintela@redhat.com>
    Acked-by: Dan Kenigsberg <danken@redhat.com>
    Acked-by: Donald Dutile <ddutile@redhat.com>
    Message-ID: <87y6q5ol37.fsf@pike.pond.sub.org>
    Signed-off-by: Eduardo Habkost <ehabkost@redhat.com>
    Obsoletes: <877hxqyp2z.fsf@pike.pond.sub.org>
    Acked-by: Amit Shah <amit.shah@redhat.com>

--- qemu/block.c
+++ qemu/block.c
@@ -180,7 +180,7 @@ BlockDriver *bdrv_find_format(const char *format_name)
 static int bdrv_is_supported(BlockDriver *drv)
 {
     static const char *supported[] = {
-        "raw", "qcow2", "host_device", NULL
+        "raw", "qcow2", "host_device", "vvfat", NULL
     };
     const char **p;
 
